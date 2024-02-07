261e490f902bb3acec9a858d38b230fa9afeac4e
yasminfranco <yasfranco98@gmail.com>
2024-02-07 15:03:56 -0300

    obteniendo la data de la API

--- src/model/elixirModel.ts
+++ src/model/elixirModel.ts
@@ -1,2 +1,13 @@
 import dotenv from "dotenv"
-dotenv.config()
+dotenv.config();
+
+const apiUrl = process.env.URL_API!;
+const api = new URL(apiUrl);
+
+const getAllData = async () => {
+    const response = await fetch(api);
+    const data = await response.json();
+    console.log(data)
+}
+
+getAllData();
