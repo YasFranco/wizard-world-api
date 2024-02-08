8f1e88ff4cdf7fde67ea6f595f2e1673af96d5bf
yasminfranco <yasfranco98@gmail.com>
2024-02-08 13:59:02 -0300

    Creación de metodo para buscar por dificultad

--- src/model/elixirModel.ts
+++ src/model/elixirModel.ts
@@ -52,14 +52,33 @@ const getEffectElixirById = async (id: string) => {
 
 }
 
+const getElixirsByDifficulty = async (difficulty: string) => {
+    try {
+        const data = await getAllData();
+        // const difficultyLowerCase = elixir.difficulty.toLowerCase()
+        const difficultyLowerCase = difficulty.toLowerCase();
 
+        const searchbydifficulty = data.filter((elixir:any) => elixir.difficulty.toLowerCase() === difficultyLowerCase);
+        
+        if(searchbydifficulty.length === 0){
+            throw new Error ("dificultad no encontrada")
+        }else{
+            return searchbydifficulty
+        }
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 const main = async () => {
     const allData = await getAllData();
     const elixirNames = await getAllElixirNames();
     const elixirEffect = await getEffectElixirById("0106fb32-b00d-4d70-9841-4b7c2d2cca71")
+    const elixirByDifficulty = await getElixirsByDifficulty("unkown")
 
-    console.log(elixirEffect)
+    console.log(elixirByDifficulty)
 }
 
 main();
