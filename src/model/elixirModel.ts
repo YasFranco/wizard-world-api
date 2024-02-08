be7ddbf8d22f8c402eca501ab1c587d8a8cc025f
yasminfranco <yasfranco98@gmail.com>
2024-02-08 14:59:08 -0300

    Creación de método para buscar los ingredientes de un elixir

--- src/model/elixirModel.ts
+++ src/model/elixirModel.ts
@@ -68,7 +68,36 @@ const getElixirsByDifficulty = async (difficulty: string) => {
 
 
     } catch (error) {
-        console.log(error)
+        return(error)
+    }
+}
+
+const getElixirsIngredientsById = async (id: string) => {
+    try{
+        const data = await getAllData();
+
+        const elixirIngredients = data.find((elixir:any) => elixir.id === id);
+        //aca va a devolver un array del elixir 
+
+        if(!elixirIngredients){
+            throw new Error ("elixir no encontrado, ID incorrecto");
+        } 
+        // aca controlo el error de no encontrar el elixir con ese id 
+
+        const arrayIngredients = elixirIngredients.ingredients;
+        // console.log(arrayIngredients)
+        // aca guardo en una variable el array de ingredientes
+
+        if(arrayIngredients.length === 0){
+            throw new Error("Este elixir no tiene ingredientes");
+            // aca controlo el error en el caso de que el array este vacio 
+        } else{
+            const newArrayIngredients = arrayIngredients.map((ingredient:any) => ingredient.name)
+            return newArrayIngredients;
+        }
+
+    }catch(error){
+        return error
     }
 }
 
@@ -77,8 +106,9 @@ const main = async () => {
     const elixirNames = await getAllElixirNames();
     const elixirEffect = await getEffectElixirById("0106fb32-b00d-4d70-9841-4b7c2d2cca71")
     const elixirByDifficulty = await getElixirsByDifficulty("unkown")
+    const elixirIngredients = await getElixirsIngredientsById("3f8fd398-c7fa-40a9-a8c7-212a8427cb6a")
 
-    console.log(elixirByDifficulty)
+    console.log(elixirIngredients)
 }
 
 main();
